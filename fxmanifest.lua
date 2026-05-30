c372caaa5fabb3ca9c7388c59fcf06bd607ce64e
hassanjahoui-lang <hassan.jahoui@persolinostrocchi.istruzioneer.it>
2026-05-30 09:31:08 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CrazieFan'
+description 'Sistema di Anticheat Di Stonebridge Bay Roleplay' -- Non modificabile per questi di copyright
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
